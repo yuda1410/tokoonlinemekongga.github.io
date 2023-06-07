cfbe9c03443f5e2dc5d113b5b3dcf082e1280412
yuda ferdiansyah <104342272+yuda1410@users.noreply.github.com>
2023-06-07 20:23:59 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Virgin Coconut Oil (VCO) atau minyak kelapa murni merupakan salah satu produk yang terbuat dari buah kelapa (Cocos nucifera). VCO menjadi produk yang sangat dicari karena khasiatnya. Berbagai macam penyakit dapat dicegah dengan mengkonsumsi VCO karena adanya kandungan asam lemak rantai sedang, seperti asam laurat. Para pengusaha pun belomba-lomba untuk memproduksinya karena prospeknya yang menjanjikan.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #ff8680;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #ff8680"><b>TOKO ONLINE MEKONGGA</b></h3>
+					<p>Jl. KH. AHMAD DAHLAN</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6287804616097</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> mekongga20@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #ff8680; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; MEKONGGA20</span>
+		</div>
+	</footer>
+
+</body>
+</html>
